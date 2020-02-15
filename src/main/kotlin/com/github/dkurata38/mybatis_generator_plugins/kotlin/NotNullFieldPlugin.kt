260c4851a0e1302baef0d52e8b005a4b28ffd8e2
dkurata38 <d.kruata.38@gmail.com>
2020-02-16 01:01:56 +0900

    code polish

--- src/main/kotlin/com/github/dkurata38/mybatis_generator_plugins/kotlin/NotNullFieldPlugin.kt
+++ src/main/kotlin/com/github/dkurata38/mybatis_generator_plugins/kotlin/NotNullFieldPlugin.kt
@@ -3,65 +3,57 @@ package com.github.dkurata38.mybatis_generator_plugins.kotlin
 import org.mybatis.generator.api.IntrospectedTable
 import org.mybatis.generator.api.PluginAdapter
 import org.mybatis.generator.api.dom.kotlin.KotlinFile
-import org.mybatis.generator.api.dom.kotlin.KotlinModifier
 import org.mybatis.generator.api.dom.kotlin.KotlinProperty
 import org.mybatis.generator.api.dom.kotlin.KotlinType
 
-class NotNullFieldPlugin: PluginAdapter() {
-    override fun validate(warnings: MutableList<String>?): Boolean {
-        return true
-    }
+class NotNullFieldPlugin : PluginAdapter() {
+	override fun validate(warnings: MutableList<String>?): Boolean {
+		return true
+	}
 
-    override fun kotlinDataClassGenerated(kotlinFile: KotlinFile?, dataClass: KotlinType?, introspectedTable: IntrospectedTable?): Boolean {
-        if (kotlinFile == null || dataClass == null || introspectedTable == null) {
-            return super.kotlinDataClassGenerated(kotlinFile, dataClass, introspectedTable)
-        }
+	override fun kotlinDataClassGenerated(kotlinFile: KotlinFile?, dataClass: KotlinType?, introspectedTable: IntrospectedTable?): Boolean {
+		if (kotlinFile == null || dataClass == null || introspectedTable == null) {
+			return super.kotlinDataClassGenerated(kotlinFile, dataClass, introspectedTable)
+		}
 
-        if (introspectedTable.context.targetRuntime != "MyBatis3Kotlin") {
-            return super.kotlinDataClassGenerated(kotlinFile, dataClass, introspectedTable)
-        }
+		if (introspectedTable.context.targetRuntime != "MyBatis3Kotlin") {
+			return super.kotlinDataClassGenerated(kotlinFile, dataClass, introspectedTable)
+		}
 
-        val properties = dataClass.constructorProperties
-                .filterNotNull()
-                .filterNot { isNullableProperty(it, introspectedTable) }
-                .map { property ->
-                    val dataType =
-                            property.dataType.orElse("").removeSuffix("?")
-                    property.initializationString
-                            .map { generateKotlinProperty(property.name, dataType, property.modifiers, property.type, it) }
-                            .orElseGet { generateKotlinProperty(property.name, dataType, property.modifiers, property.type) }
-                }
-        dataClass.constructorProperties.replaceAll {
-            val property = properties
-                    .find { kotlinProperty -> it.name == kotlinProperty.name }
-            property?: it
-        }
-        return super.kotlinDataClassGenerated(kotlinFile, dataClass, introspectedTable)
-    }
+		val properties = dataClass.constructorProperties
+				.filterNotNull()
+				.filterNot { isNullableProperty(it, introspectedTable) }
+				.map { it.toImmutableProperty() }
 
-    fun isNullableProperty(kotlinProperty: KotlinProperty, introspectedTable: IntrospectedTable): Boolean {
-        return introspectedTable
-                .allColumns
-                .find { kotlinProperty.name == it.javaProperty }
-                ?.isNullable!!
-    }
+		dataClass.constructorProperties.replaceAll {
+			val property = properties
+					.find { kotlinProperty -> it.name == kotlinProperty.name }
+			property ?: it
+		}
+		return super.kotlinDataClassGenerated(kotlinFile, dataClass, introspectedTable)
+	}
 
-    fun generateKotlinProperty(propertyName: String, dataType: String, modifiers: List<KotlinModifier>, type: KotlinProperty.Type, initializationString: String): KotlinProperty {
-        var builder = if (type == KotlinProperty.Type.VAL) KotlinProperty.newVal(propertyName) else KotlinProperty.newVar(propertyName)
-        builder = builder
-                .withDataType(dataType)
-                .withInitializationString(initializationString)
-        modifiers
-                .forEach{ builder = builder.withModifier(it) }
-        return builder.build()
-    }
+	/**
+	 * 第一引数で指定したKotlinDataClassのコンストラクタパラメータがNotNullカラムに対応したものかどうかを判定する。
+	 */
+	fun isNullableProperty(kotlinProperty: KotlinProperty, introspectedTable: IntrospectedTable): Boolean {
+		return introspectedTable
+				.allColumns
+				.find { kotlinProperty.name == it.javaProperty }
+				?.isNullable!!
+	}
 
-    fun generateKotlinProperty(propertyName: String, dataType: String, modifiers: List<KotlinModifier>, type: KotlinProperty.Type): KotlinProperty {
-        var builder = if (type == KotlinProperty.Type.VAL) KotlinProperty.newVal(propertyName) else KotlinProperty.newVar(propertyName)
-        builder = builder
-                .withDataType(dataType)
-        modifiers
-                .forEach{ builder = builder.withModifier(it) }
-        return builder.build()
-    }
+	/**
+	 * デフォルトで生成されるKotlinDataClassのコンストラクタパラメータをもとに
+	 * イミュータブルなコンストラクタパラメータを生成する。
+	 * 再代入不可能な型はNonNull型でパラメータ名はデフォルトで生成されるものと同様。
+	 */
+	fun KotlinProperty.toImmutableProperty(): KotlinProperty {
+		val dataType = this.dataType.orElse("").removeSuffix("?")
+		var builder = KotlinProperty.newVal(this.name)
+				.withDataType(dataType)
+		this.modifiers
+				.forEach { builder = builder.withModifier(it) }
+		return builder.build()
+	}
 }
