c327ffc37a9a7f11f1bb2bc50ba806b6645026cd
dkurata38 <d.kruata.38@gmail.com>
2020-03-10 00:03:20 +0900

    MyBatis3*SQLMAPPERで動作するよう修正

--- src/main/kotlin/com/github/dkurata38/mybatis_generator_plugins/kotlin/SeparateSubPackagesPlugin.kt
+++ src/main/kotlin/com/github/dkurata38/mybatis_generator_plugins/kotlin/SeparateSubPackagesPlugin.kt
@@ -14,34 +14,62 @@ class SeparateSubPackagesPlugin : PluginAdapter() {
 
 	override fun initialized(introspectedTable: IntrospectedTable) {
 		val tableName = introspectedTable.fullyQualifiedTable.fullyQualifiedTableNameAtRuntime
-		val fullyQualifiedTable = introspectedTable.fullyQualifiedTable
-		val originalDomainObjectName = fullyQualifiedTable.domainObjectName
-		val newDomainObjectName = originalDomainObjectName.plus(".$tableName")
-		val tableConfiguration = introspectedTable.tableConfiguration
-		val delimitIdentifiers = tableConfiguration.isDelimitIdentifiers
-				|| stringContainsSpace(tableConfiguration.catalog)
-				|| stringContainsSpace(tableConfiguration.schema)
-				|| stringContainsSpace(tableConfiguration.tableName)
-		introspectedTable.fullyQualifiedTable =
-				FullyQualifiedTable(
-						fullyQualifiedTable.introspectedCatalog,
-						fullyQualifiedTable.introspectedSchema,
-						fullyQualifiedTable.introspectedTableName,
-						newDomainObjectName,
-						fullyQualifiedTable.alias,
-						isTrue(tableConfiguration.getProperty(PropertyRegistry.TABLE_IGNORE_QUALIFIERS_AT_RUNTIME)),
-						tableConfiguration.getProperty(PropertyRegistry.TABLE_RUNTIME_CATALOG),
-						tableConfiguration.getProperty(PropertyRegistry.TABLE_RUNTIME_SCHEMA),
-						tableConfiguration.getProperty(PropertyRegistry.TABLE_RUNTIME_TABLE_NAME),
-						delimitIdentifiers,
-						null,
-						context
-				)
-		introspectedTable.myBatisDynamicSqlSupportType = typeToSubpackageType(introspectedTable.myBatisDynamicSqlSupportType, tableName)
-		super.initialized(introspectedTable)
+//		val fullyQualifiedTable = introspectedTable.fullyQualifiedTable
+//		val originalDomainObjectName = fullyQualifiedTable.domainObjectName
+//		val newDomainObjectName = originalDomainObjectName.plus(".$tableName")
+//		val tableConfiguration = introspectedTable.tableConfiguration
+//		val delimitIdentifiers = tableConfiguration.isDelimitIdentifiers
+//				|| stringContainsSpace(tableConfiguration.catalog)
+//				|| stringContainsSpace(tableConfiguration.schema)
+//				|| stringContainsSpace(tableConfiguration.tableName)
+//		introspectedTable.fullyQualifiedTable =
+//				FullyQualifiedTable(
+//						fullyQualifiedTable.introspectedCatalog,
+//						fullyQualifiedTable.introspectedSchema,
+//						fullyQualifiedTable.introspectedTableName,
+//						newDomainObjectName,
+//						fullyQualifiedTable.alias,
+//						isTrue(tableConfiguration.getProperty(PropertyRegistry.TABLE_IGNORE_QUALIFIERS_AT_RUNTIME)),
+//						tableConfiguration.getProperty(PropertyRegistry.TABLE_RUNTIME_CATALOG),
+//						tableConfiguration.getProperty(PropertyRegistry.TABLE_RUNTIME_SCHEMA),
+//						tableConfiguration.getProperty(PropertyRegistry.TABLE_RUNTIME_TABLE_NAME),
+//						delimitIdentifiers,
+//						null,
+//						context
+//				)
+//		if (introspectedTable.context.javaModelGeneratorConfiguration != null) {
+//			introspectedTable.context.javaModelGeneratorConfiguration.targetPackage = typeToSubpackageType(introspectedTable.context.javaModelGeneratorConfiguration.targetPackage, tableName)
+//		}
+//		if (introspectedTable.context.javaClientGeneratorConfiguration != null) {
+//			introspectedTable.context.javaClientGeneratorConfiguration.targetPackage = typeToSubpackageType(introspectedTable.context.javaClientGeneratorConfiguration.targetPackage, tableName)
+//		}
+//		if (introspectedTable.context.sqlMapGeneratorConfiguration != null) {
+//			introspectedTable.context.sqlMapGeneratorConfiguration.targetPackage = typeToSubpackageType(introspectedTable.context.sqlMapGeneratorConfiguration.targetPackage, tableName)
+//		}
+		if (introspectedTable.baseRecordType != null) {
+			introspectedTable.baseRecordType = typeToSubpackageType(introspectedTable.baseRecordType, tableName)
+		}
+		if (introspectedTable.myBatis3JavaMapperType != null) {
+			introspectedTable.myBatis3JavaMapperType = typeToSubpackageType(introspectedTable.myBatis3JavaMapperType, tableName)
+		}
+		if (introspectedTable.exampleType != null) {
+			introspectedTable.exampleType = typeToSubpackageType(introspectedTable.exampleType, tableName)
+		}
+		if (introspectedTable.myBatis3XmlMapperPackage != null) {
+			introspectedTable.myBatis3XmlMapperPackage = introspectedTable.myBatis3XmlMapperPackage + "." + tableName
+		}
+//		if (introspectedTable.myBatis3FallbackSqlMapNamespace != null) {
+//			introspectedTable.myBatis3FallbackSqlMapNamespace = typeToSubpackageType(introspectedTable.myBatis3FallbackSqlMapNamespace, tableName)
+//		}
+//		if (introspectedTable.myBatis3SqlMapNamespace != null) {
+//			introspectedTable.myBatis3FallbackSqlMapNamespace = typeToSubpackageType(introspectedTable.myBatis3SqlMapNamespace, tableName)
+//		}
+//		introspectedTable.myBatisDynamicSqlSupportType = typeToSubpackageType(introspectedTable.myBatisDynamicSqlSupportType, tableName)
+//		super.initialized(introspectedTable)
 	}
 
 	private fun typeToSubpackageType(type: String, subPackageName: String): String {
 		return type.replace("([^.]+)$".toRegex(), "$subPackageName.$1")
 	}
+
 }
