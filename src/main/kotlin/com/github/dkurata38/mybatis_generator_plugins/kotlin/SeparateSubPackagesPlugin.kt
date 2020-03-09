e4ba1de79936dbf4f8fa1ef2edf96ab12b058d2f
dkurata38 <d.kruata.38@gmail.com>
2020-03-10 00:57:38 +0900

    コメントを削除

--- src/main/kotlin/com/github/dkurata38/mybatis_generator_plugins/kotlin/SeparateSubPackagesPlugin.kt
+++ src/main/kotlin/com/github/dkurata38/mybatis_generator_plugins/kotlin/SeparateSubPackagesPlugin.kt
@@ -1,11 +1,7 @@
 package com.github.dkurata38.mybatis_generator_plugins.kotlin
 
-import org.mybatis.generator.api.FullyQualifiedTable
 import org.mybatis.generator.api.IntrospectedTable
 import org.mybatis.generator.api.PluginAdapter
-import org.mybatis.generator.config.PropertyRegistry
-import org.mybatis.generator.internal.util.StringUtility.isTrue
-import org.mybatis.generator.internal.util.StringUtility.stringContainsSpace
 
 class SeparateSubPackagesPlugin : PluginAdapter() {
 	override fun validate(warnings: MutableList<String>?): Boolean {
@@ -14,38 +10,6 @@ class SeparateSubPackagesPlugin : PluginAdapter() {
 
 	override fun initialized(introspectedTable: IntrospectedTable) {
 		val tableName = introspectedTable.fullyQualifiedTable.fullyQualifiedTableNameAtRuntime
-//		val fullyQualifiedTable = introspectedTable.fullyQualifiedTable
-//		val originalDomainObjectName = fullyQualifiedTable.domainObjectName
-//		val newDomainObjectName = originalDomainObjectName.plus(".$tableName")
-//		val tableConfiguration = introspectedTable.tableConfiguration
-//		val delimitIdentifiers = tableConfiguration.isDelimitIdentifiers
-//				|| stringContainsSpace(tableConfiguration.catalog)
-//				|| stringContainsSpace(tableConfiguration.schema)
-//				|| stringContainsSpace(tableConfiguration.tableName)
-//		introspectedTable.fullyQualifiedTable =
-//				FullyQualifiedTable(
-//						fullyQualifiedTable.introspectedCatalog,
-//						fullyQualifiedTable.introspectedSchema,
-//						fullyQualifiedTable.introspectedTableName,
-//						newDomainObjectName,
-//						fullyQualifiedTable.alias,
-//						isTrue(tableConfiguration.getProperty(PropertyRegistry.TABLE_IGNORE_QUALIFIERS_AT_RUNTIME)),
-//						tableConfiguration.getProperty(PropertyRegistry.TABLE_RUNTIME_CATALOG),
-//						tableConfiguration.getProperty(PropertyRegistry.TABLE_RUNTIME_SCHEMA),
-//						tableConfiguration.getProperty(PropertyRegistry.TABLE_RUNTIME_TABLE_NAME),
-//						delimitIdentifiers,
-//						null,
-//						context
-//				)
-//		if (introspectedTable.context.javaModelGeneratorConfiguration != null) {
-//			introspectedTable.context.javaModelGeneratorConfiguration.targetPackage = typeToSubpackageType(introspectedTable.context.javaModelGeneratorConfiguration.targetPackage, tableName)
-//		}
-//		if (introspectedTable.context.javaClientGeneratorConfiguration != null) {
-//			introspectedTable.context.javaClientGeneratorConfiguration.targetPackage = typeToSubpackageType(introspectedTable.context.javaClientGeneratorConfiguration.targetPackage, tableName)
-//		}
-//		if (introspectedTable.context.sqlMapGeneratorConfiguration != null) {
-//			introspectedTable.context.sqlMapGeneratorConfiguration.targetPackage = typeToSubpackageType(introspectedTable.context.sqlMapGeneratorConfiguration.targetPackage, tableName)
-//		}
 		// MyBatis3
 		if (introspectedTable.baseRecordType != null) {
 			introspectedTable.baseRecordType = typeToSubpackageType(introspectedTable.baseRecordType, tableName)
@@ -77,9 +41,6 @@ class SeparateSubPackagesPlugin : PluginAdapter() {
 		if (introspectedTable.kotlinRecordType != null) {
 			introspectedTable.kotlinRecordType = typeToSubpackageType(introspectedTable.kotlinRecordType, tableName)
 		}
-
-//		introspectedTable.myBatisDynamicSqlSupportType = typeToSubpackageType(introspectedTable.myBatisDynamicSqlSupportType, tableName)
-//		super.initialized(introspectedTable)
 	}
 
 	private fun typeToSubpackageType(type: String, subPackageName: String): String {
