ac7a8c3d0b807af6d13f1e8b0fd1356ad71867b7
Kuchita El <el.kuchita@gmail.com>
2024-09-05 23:57:39 +0900

    ToNonnullProperty関数のテストを実装

--- mybatis-generator-plugins-core/src/test/kotlin/com/github/dkurata38/mybatis_generator_plugins/kotlin/NotNullFieldPluginTest.kt
+++ mybatis-generator-plugins-core/src/test/kotlin/com/github/dkurata38/mybatis_generator_plugins/kotlin/NotNullFieldPluginTest.kt
@@ -1,35 +1,61 @@
 package com.github.dkurata38.mybatis_generator_plugins.kotlin
 
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.mybatis.generator.api.IntrospectedColumn
+import org.mybatis.generator.api.dom.kotlin.KotlinModifier
 import org.mybatis.generator.api.dom.kotlin.KotlinProperty
 import org.mybatis.generator.runtime.kotlin.IntrospectedTableKotlinImpl
-import java.util.stream.Stream
+import java.util.Optional
 
 class NotNullFieldPluginTest {
     @Nested
     inner class ToNonnullPropertyTest {
-        @TestFactory
-        fun `プロパティのデータ型がString型になる`(): Stream<DynamicTest> = DynamicTest.stream(
-            listOf(
-                KotlinProperty.newVar("name")
-                    .withDataType("String")
-                    .build(),
-                KotlinProperty.newVar("name")
-                    .withDataType("String?")
-                    .withInitializationString("null")
-                    .build()
-            ).stream(),
-            { "元のデータ型が${it.dataType.get()}の場合" },
-            {
-                val actual = NotNullFieldPlugin.toNonnullProperty(it)
-                assertEquals("String", actual.dataType.get())
-            }
+
+        @ParameterizedTest
+        @CsvSource(
+            "VAL, field1, String?, PRIVATE, @Getter, defaultString, String, defaultString", // VAL、 元がNullable、 デフォルト値がNULLではない
+            "VAR, field2, Int, PRIVATE, @Setter, 0, Int, 0", // VAR、元がNotNull、デフォルト値がNULL以外
+            "VAL, field3, String?, PRIVATE, @Getter, null, String, NULL", // VAL、元がNullable、デフォルト値がnull
+            "VAL, field4, String, NULL, NULL, NULL, String, NULL", // VAL、元がNotNull、オプションのパラメータが全部Null
+            nullValues = ["NULL"]
         )
+        fun プロパティのデータ型がNotNull型になる(
+            type: KotlinProperty.Type,
+            name: String,
+            dataType: String,
+            modifier: KotlinModifier?,
+            annotation: String?,
+            initializationString: String?,
+            expectedDataType: String,
+            expectedInitializationString: String?
+        ) {
+            var builder =
+                if (type == KotlinProperty.Type.VAL) KotlinProperty.newVal(name) else KotlinProperty.newVar(name)
+            builder = builder
+                .withDataType(dataType)
+            if (modifier != null) builder = builder.withModifier(modifier)
+            if (annotation != null) builder = builder.withAnnotation(annotation)
+            if (initializationString != null) builder = builder.withInitializationString(initializationString)
+
+            val p = builder.build()
+            val expectedModifier = listOfNotNull(modifier).toTypedArray()
+            val expectedAnnotation = listOfNotNull(annotation).toTypedArray()
+
+            val actual = NotNullFieldPlugin.toNonnullProperty(p)
+
+            assertAll(
+                { assertEquals(type, actual.type) },
+                { assertEquals(name, actual.name) },
+                { assertEquals(Optional.ofNullable(expectedDataType), actual.dataType) },
+                { assertArrayEquals(expectedModifier, actual.modifiers.toTypedArray()) },
+                { assertArrayEquals(expectedAnnotation, actual.annotations.toTypedArray()) },
+                { assertEquals(Optional.ofNullable(expectedInitializationString), actual.initializationString) }
+            )
+        }
     }
 
     @Nested
